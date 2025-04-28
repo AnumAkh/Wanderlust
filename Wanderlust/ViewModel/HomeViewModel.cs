9733752df0b3877086e6021d813bde86505b5a2b
AnumAkh <anumnavaid4@gmail.com>
2025-04-28 07:18:10 -0700

    Destination page and destination detail done. booking left. wishlist in progress

---
+++ Wanderlust/ViewModel/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Wanderlust.Models;
+
+namespace Wanderlust.ViewModel
+{
+    public class HomeViewModel
+    {
+        public List<DESTINATION> FeaturedDestinations { get; set; }
+        public List<PACKAGE> FeaturedPackages { get; set; }
+    }
+}
